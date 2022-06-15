4d67b3636631e8947fef56d03af9f184948d8dd5
Владимир Ворона <vvorona665@gmail.com>
2022-06-15 23:02:09 +0300

    Completed task 2

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа принимает на вход трехзначное число и на выходе показывает 2 цифру этого числа 
+Console.WriteLine("Введите трехназчное число  ");
+int Namber = Convert.ToInt32(Console.ReadLine());
+
+int result = Namber / 10;
+int result2 = result % 10;
+
+Console.WriteLine(result2);
