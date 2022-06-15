708e9025b2edeaeb29c9341dbef998ee6e1fe073
Владимир Ворона <vvorona665@gmail.com>
2022-06-15 23:45:49 +0300

    Completed task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа которая принимает на вход число и показывает 3 цирфу этого числа, еслии ее нет, то выводит "Третьей цифры нет"
+Console.WriteLine("Введите число  ");
+int Namber = Convert.ToInt32(Console.ReadLine());
+
+if (Namber > 99 & Namber <= 999)
+{
+    int result = Namber % 10;
+    Console.WriteLine(result);
+}
+if (Namber > 999)
+{
+    int result2 = Namber % 100;
+    int result3 = result2 / 10;
+    Console.WriteLine(result3);
+}
+
+if (Namber <= 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
